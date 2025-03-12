2f0126dfc0eed4a1b881404666d33f4e8b9f8faf
junho kim <kesu9251@naver.com>
2025-03-12 22:31:44 +0900

    좌석 찾고 클릭

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 if __name__ == '__main__':
 
     driver = Driver(uc=True)
+    seat = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "circle[id='seat_block_25002656\\:24001584\\:001\\:424']"))
+    )
+    # ✅ ActionChains를 사용하여 클릭 (권장)
+    ActionChains(driver).move_to_element(seat).click().perform()
+
     button = driver.find_element("xpath",
         "//button[contains(@class, 'EntButton_button__bdl_j') and contains(@class, 'EntButton_primary__UOX1_')]")
     button.click()
